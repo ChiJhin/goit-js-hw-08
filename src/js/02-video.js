6b511dad8817c80aa45dfe06191fb71f38e81e5c
ChiJhin <jerek.d.lem@gmail.com>
2023-11-17 21:28:23 +0200

    task-2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,16 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('#vimeo-player');
+
+const player = new Player(iframe);
+
+function saveTime({ seconds }) {
+  localStorage.setItem('videoplayer-current-time', seconds);
+}
+
+player.on('timeupdate', throttle(saveTime, 300));
+
+const currentTime = localStorage.getItem('videoplayer-current-time');
+
+player.setCurrentTime(currentTime || 0);
